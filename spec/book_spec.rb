da9d52781d597b7e1b1d6a262a71e3bba23563bc
Sebastian McKee <sebmcke@gmail.com>
2025-01-08 19:55:23 -0600

    Add initialize test

---
+++ spec/book_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require './lib/book'
+
+describe Book do
+    before do
+        @book = Book.new({
+            author_first_name: "Harper", 
+            author_last_name: "Lee", 
+            title: "To Kill a Mockingbird", 
+            publication_date: "July 11, 1960"
+        })
+    end
+
+    describe '#initialize' do
+        it 'can initialize a book' do
+            expect(@book).to be_a(Book)
+            expect(@book.title).to eq("To Kill a Mockingbird")
+            expect(@book.author).to eq("Harper Lee")
+            expect(@book.publication_date).to eq("1960")
+        end
+    end
+end
